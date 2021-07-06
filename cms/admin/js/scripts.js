47959c09ff10efeae82debd18fec48f9dadacf0d
Zefeng Wang <55709954+zefengw@users.noreply.github.com>
2021-07-06 11:57:36 -0400

    Delete cms directory

--- cms/admin/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function toggle(source) {
-  let checkboxes = document.getElementsByName('checkBoxArray[]');
-  for(var i=0, n=checkboxes.length;i<n;i++) {
-    checkboxes[i].checked = source.checked;
-  }
-}
